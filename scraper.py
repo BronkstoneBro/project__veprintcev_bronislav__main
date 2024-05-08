e20475c2b40e07fa7b28f2a0f2006bf20c64a66b
BronkstoneBro <atlantwindow@gmail.com>
2024-05-08 15:54:19 +0300

    start project 🎉

---
+++ scraper.py
@@ -0,0 +1,24 @@
+import schedule
+import time
+from main import scrape_product
+
+
+def start_scrape():
+    urls = [
+        'https://autoliga.net.ua/catalog/volkswagen/-tiguan-ii-15-17/1/',
+        'https://autoliga.net.ua/catalog/porsche/-cayenne-18--9y/1/',
+        'https://autoliga.net.ua/catalog/toyota/-camry-v70-18-/1/'
+    ]
+    for url in urls:
+        print("Parsing products from", url)
+        scrape_product(url)
+        print("Parsing complete.")
+
+
+schedule.every().day.at("15:00z").do(start_scrape)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
+
+# script start scrape every day at 08:00
